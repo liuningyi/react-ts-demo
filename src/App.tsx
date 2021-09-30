b6463bab6111141f188d6977e2de18d838361c43
lvyuqing <yqlv53@best-inc.com>
2021-09-30 13:26:46 +0800

    项目结构初始化

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Table from './components/table/Table';
+function App() {
+  return (
+    <div className="App">
+      <Table></Table>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/table/Table.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class DemoOne extends Component {
+    render() {
+        return (
+            <div>
+               <table>
+                   <thead>
+                       <tr>
+                           <th>姓名</th>
+                       </tr>
+                   </thead>
+                   <tbody>
+                       <tr>
+                           <td>小方</td>
+                       </tr>
+                   </tbody>
+               </table>
+            </div>
+        )
+    }
+}
